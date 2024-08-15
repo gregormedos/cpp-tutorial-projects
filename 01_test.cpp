fec1c5b4158cf2f8654f6a847cc309a483d5fe6f
Gregor Medoš <medos.gregor@gmail.com>
2024-08-15 14:57:09 +0200

    compile and execute C++ code

---
+++ 01_test.cpp
@@ -0,0 +1,11 @@
+//preprocessor header files
+#include <iostream>
+
+
+//main function
+int main()
+{
+    std::cout << "Hello World!\n"; //C++ uses objects and methods (functions or operators)
+
+    return 0; //exit status = SUCCESS
+}
